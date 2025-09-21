d39ebb4c88db5158dec591bc12f1a36bdc0706d5
Richard <kiwi348750059@gmail.com>
2025-09-20 22:47:42 -0400

    red when hit
    changed mesh render color to red when opponent is hit

--- Assets/Scripts/Misc/HitDetection.cs
+++ Assets/Scripts/Misc/HitDetection.cs
@@ -1,12 +1,15 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public class HitDetection : MonoBehaviour, IDamageable
 {
-    private PlayerController _player; 
+    private PlayerController _player;
     [SerializeField] internal PlayerController otherPlayer;
     public static event Action OnDeath;
+
     private void Awake()
     {
         _player = gameObject.GetComponentInParent<PlayerController>();
@@ -19,7 +22,7 @@ public class HitDetection : MonoBehaviour, IDamageable
 
     private void OnCollisionEnter(Collision other)
     {
-       
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -39,58 +42,94 @@ public class HitDetection : MonoBehaviour, IDamageable
         {
             _player.AtBorder = true;
         }
-     
+
     }
 
     private PlayerController OnHit(PlayerController sender, PlayerController receiver)
     {
-        
+
         //Check the players buffers for last attack frame  and decide the player hit
         var attackBufferSender = sender.GetComponentInParent<InputReader>();
         var attackBufferReceiver = receiver.GetComponentInParent<InputReader>();
-        
-        Debug.Log(attackBufferSender.lastAttackInput );
-        Debug.Log(attackBufferReceiver.lastAttackInput );
-        
+
+        Debug.Log(attackBufferSender.lastAttackInput);
+        Debug.Log(attackBufferReceiver.lastAttackInput);
+
 
         if (attackBufferSender.lastAttackInput != InputReader.AttackInputResult.None &&
             attackBufferReceiver.lastAttackInput != InputReader.AttackInputResult.None)
         {
-            var result = attackBufferSender.lastAttackInputFrame < attackBufferReceiver.lastAttackInputFrame ?  sender : receiver ;
+            var result = attackBufferSender.lastAttackInputFrame < attackBufferReceiver.lastAttackInputFrame
+                ? sender
+                : receiver;
             print(result);
             return result;
-        } 
-        if (attackBufferSender.lastAttackInput != InputReader.AttackInputResult.None && attackBufferReceiver.lastAttackInput == InputReader.AttackInputResult.None)
+        }
+
+        if (attackBufferSender.lastAttackInput != InputReader.AttackInputResult.None &&
+            attackBufferReceiver.lastAttackInput == InputReader.AttackInputResult.None)
         {
             return receiver;
         }
-        if (attackBufferSender.lastAttackInput == InputReader.AttackInputResult.None && attackBufferReceiver.lastAttackInput != InputReader.AttackInputResult.None)
+
+        if (attackBufferSender.lastAttackInput == InputReader.AttackInputResult.None &&
+            attackBufferReceiver.lastAttackInput != InputReader.AttackInputResult.None)
         {
             return sender;
-        } 
+        }
+
         return null;
     }
+
 
     public void TakeDamage(int damage)
     {
         // deal damage and active death event to trigger end of game 
-        
+
         _player.Health -= damage;
 //        print(otherPlayer.name);
-  //      print(_player.name);
-
+        //      print(_player.name);
+        StartCoroutine(FlashRed(_player)); //Red when hit
         if (!_player.AtBorder)
         {
-             otherPlayer.StartCoroutine(otherPlayer.PlayerKnockBack.KnockBackOtherPlayer(_player));
+            otherPlayer.StartCoroutine(otherPlayer.PlayerKnockBack.KnockBackOtherPlayer(_player));
         }
         else
         {
-            otherPlayer.StartCoroutine(otherPlayer.PlayerKnockBack.KnockBackThisPlayer(otherPlayer)); 
+            otherPlayer.StartCoroutine(otherPlayer.PlayerKnockBack.KnockBackThisPlayer(otherPlayer));
         }
-        
+
         if (_player.Health <= 0)
         {
             OnDeath?.Invoke();
+        }
+    }
+
+    private IEnumerator FlashRed(PlayerController player)//bro wtf, this took me way to long to do
+    {
+        SkinnedMeshRenderer[] renderers = player.GetComponentsInChildren<SkinnedMeshRenderer>();
+        MaterialPropertyBlock block = new MaterialPropertyBlock();
+
+        foreach (var render in renderers)
+        {
+            if (render != null)
+            {
+                render.GetPropertyBlock(block);
+                block.SetColor("_BaseColor", Color.red);
+                render.SetPropertyBlock(block);
+            }
+        }
+
+        yield return new WaitForSeconds(0.1f);
+
+        foreach (var render in renderers)
+        {
+            if (render != null)
+            {
+                render.GetPropertyBlock(block);
+                block.SetColor("_BaseColor", Color.white);
+                render.SetPropertyBlock(block);
+            }
         }
     }
 }
--- Assets/Scripts/PlayerStateMachine/Attack/PlayerKnockBack.cs
+++ Assets/Scripts/PlayerStateMachine/Attack/PlayerKnockBack.cs
@@ -7,14 +7,14 @@ public class PlayerKnockBack : MonoBehaviour
     private const float KnockBackTime = 0.1f;
     private float _hitDirectionForce;
 
-    public IEnumerator KnockBackOtherPlayer( PlayerController player)
+    public IEnumerator KnockBackOtherPlayer(PlayerController player)
     {
         //Use this to knock back the other player 
         _isBeingKnockedBack = true;
 
-        var  hitDir = ReturnHitDir(player.PlayerHitDetection.otherPlayer) ;
+        var hitDir = ReturnHitDir(player.PlayerHitDetection.otherPlayer);
         _hitDirectionForce = ReturnHitForce(player.PlayerHitDetection.otherPlayer);
-        var hitForce = hitDir * _hitDirectionForce ;
+        var hitForce = hitDir * _hitDirectionForce;
         var elapsedTime = 0f;
         while (elapsedTime < KnockBackTime)
         {
@@ -22,23 +22,24 @@ public class PlayerKnockBack : MonoBehaviour
             player.rb.linearVelocity = hitForce;
             yield return new WaitForFixedUpdate();
         }
+
         _isBeingKnockedBack = false;
     }
-    
+
     private Vector3 ReturnHitDir(PlayerController player)
     {
         // depending on the players direction return the direction 
         return !player.Reversed ? Vector3.right : Vector3.left;
     }
-    
-    public IEnumerator KnockBackThisPlayer( PlayerController player)
+
+    public IEnumerator KnockBackThisPlayer(PlayerController player)
     {
         _isBeingKnockedBack = true;
         //Use this to knock back the attacking player 
 
-        var  hitDir = ReturnHitDir(player.PlayerHitDetection.otherPlayer) ;
+        var hitDir = ReturnHitDir(player.PlayerHitDetection.otherPlayer);
         _hitDirectionForce = ReturnHitForce(player);
-        var hitForce = hitDir * _hitDirectionForce ;
+        var hitForce = hitDir * _hitDirectionForce;
         var elapsedTime = 0f;
         while (elapsedTime < KnockBackTime)
         {
@@ -46,10 +47,11 @@ public class PlayerKnockBack : MonoBehaviour
             player.rb.linearVelocity = hitForce;
             yield return new WaitForFixedUpdate();
         }
+
         _isBeingKnockedBack = false;
     }
-    
-    
+
+
     private float ReturnHitForce(PlayerController player)
     {
         //Depending on the attack type return a knockback force value  (note mod this to add directional values later)
@@ -80,11 +82,10 @@ public class PlayerKnockBack : MonoBehaviour
             case InputReader.AttackInputResult.HeavyLeft:
                 break;
             case InputReader.AttackInputResult.HeavyRight:
-                break; 
+                break;
         }
+
         print(hitForceTemp);
         return hitForceTemp;
     }
-
-    
 }
