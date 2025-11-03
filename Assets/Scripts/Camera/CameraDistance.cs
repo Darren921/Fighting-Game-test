a2a215e5b0a68c3a81b5c424d04a84f2516c74c3
Alex-Hamblin <alexmachamblin@gmail.com>
2025-11-03 14:55:48 -0500

    camera changes

--- Assets/Scripts/Camera/CameraDistance.cs
+++ Assets/Scripts/Camera/CameraDistance.cs
@@ -1,18 +1,45 @@
+using System.Net.NetworkInformation;
 using UnityEngine;
 
 public class CameraDistance : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] GameObject pos;
-    [SerializeField] float distance;
-    void Start()
-    {
-        
-    }
+    
+        public Transform fighter1;
+        public Transform fighter2;
+        public float zoomSpeed = 2f;
+        public float minZoom = 5f;
+        public float maxZoom = 15f;
+        public float margin = 2f;
+        public float scale;
 
-    // Update is called once per frame
-    void Update()
-    {
-        distance = Vector3.Distance(gameObject.transform.position, pos.transform.position);
-    }
+        private Camera cam;
+        [SerializeField ]Vector3 playerView;
+    
+    
+
+    void Start()
+        {
+            cam = GetComponent<Camera>();
+        }
+
+        void Update()
+        {
+            float dis = Vector3.Distance(fighter1.position, fighter2.position) * scale;
+            if (dis < minZoom)
+            {
+                cam.fieldOfView = minZoom;
+            }
+            else if (dis > maxZoom)
+
+            {
+                cam.fieldOfView = maxZoom;
+            }
+            else if (dis < maxZoom && dis > minZoom)
+            {
+                cam.fieldOfView = dis;
+            }
+            playerView = cam.WorldToViewportPoint(fighter1.position);
+            
+        }
+    
 }
