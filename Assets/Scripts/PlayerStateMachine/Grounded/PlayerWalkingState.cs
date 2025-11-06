509732f169834537ef1312184cbbafe015624268
Darren921 <darren.chung@ontariotechu.net>
2025-11-05 22:54:16 -0500

    walking switched to blend tree

--- Assets/Scripts/PlayerStateMachine/Grounded/PlayerWalkingState.cs
+++ Assets/Scripts/PlayerStateMachine/Grounded/PlayerWalkingState.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 public class PlayerWalkingState : PlayerMovingState
 {
     protected override float MoveSpeed => Player.WalkSpeed;
+    private float temp;
     protected override void ApplyVelocity(PlayerController player)
     {
         var velocity = new Vector3(player.PlayerMove.x * MoveSpeed, player.rb.linearVelocity.y);
-        player.rb.linearVelocity = velocity;    
+        player.rb.linearVelocity = velocity;
+        if (!player.Animator.GetCurrentAnimatorStateInfo(0).IsName("Walking")) return;
+        temp = player.rb.linearVelocity.x switch
+        {
+            < 0 when player.Reversed => 1,
+            > 0 when player.Reversed => 0,
+            _ => player.rb.linearVelocity.x switch
+            {
+                > 0 => 1,
+                < 0 => 0,
+            }
+        };
+        player.Animator.SetFloat("WalkDir", temp);
+
+
     }
 
     internal override void UpdateState(PlayerStateManager playerStateManager, PlayerController player)
